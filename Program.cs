d4d034811c83b8a37be99fbd0283ff76392bd038
Herman Benao <hermben@gmail.com>
2021-08-22 22:41:38 -0700

    adds prompt for user to enter data

--- Program.cs
+++ Program.cs
@@ -8,6 +8,14 @@ namespace calculator_2
         {
             int num01;
             int num02;
+
+            Console.WriteLine("Please enter your first number : ");
+            num01 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("please enter your second number: ");
+            num02 = Convert.ToInt32(Console.ReadLine());
+
         }
     }
 }
